749137e31d10a5c06e60381fca9aed1cce8481fb
valentin mardoukhaev <mardoukhaevvalentin@MacBook-Pro-de-valentin.local>
2025-05-26 23:37:33 +0200

    menu burger

--- script.js
+++ script.js
@@ -18,18 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   });
 
-  modal.onclick = function () {
-    modal.classList.remove('flex');
-    modalImg.src = '';
-  };
-});
-
-    // Sinon, on ouvre la modale
-    modal.classList.add('flex');
-    modalImg.src = this.src;
-  };
-});
-
   // Empêche la fermeture si on clique sur l'image elle-même
   modalImg.onclick = (e) => e.stopPropagation();
 
@@ -39,14 +27,16 @@ document.addEventListener('DOMContentLoaded', () => {
     modalImg.src = '';
   };
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    if (sideMenu.classList.contains('open')) {
-      sideMenu.classList.remove('open');
+  // Fermeture avec la touche Échap
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      if (sideMenu.classList.contains('open')) {
+        sideMenu.classList.remove('open');
+      }
+      if (window.getComputedStyle(modal).display !== 'none') {
+        modal.classList.remove('flex');
+        modalImg.src = '';
+      }
     }
-    if (window.getComputedStyle(modal).display !== 'none') {
-      modal.classList.remove('flex');
-      modalImg.src = '';
-    }
-  }
-})
+  });
+});
